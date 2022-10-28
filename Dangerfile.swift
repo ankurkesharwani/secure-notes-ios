9740c4159298918f40a9a180713b8c2f8c4566a1
Ankur Kesharwani <ankur.kesharwani@paysense.in>
2022-10-28 12:39:45 +0530

    Adding Danger

---
+++ Dangerfile.swift
@@ -0,0 +1,17 @@
+import Danger 
+
+let danger = Danger()
+let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
+
+let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
+
+if !changelogChanged {
+  warn("No CHANGELOG entry added.")
+}
+
+// You can use these functions to send feedback:
+message("Highlight something in the table")
+warn("Something pretty bad, but not important enough to fail the build")
+fail("Something that must be changed")
+
+markdown("Free-form markdown that goes under the table, so you can do whatever.")
